7432fc93882c673b35d9196757d0848637195b9b
Shoykat361 <shoykat15-11724@diu.edu.bd>
2023-12-17 12:49:06 +0600

    Learn encapsulation

---
+++ encapsulation.py
@@ -0,0 +1,25 @@
+class Student :
+    def __init__(self,name,age,Id):
+        self.__name = name
+        self.__Id = Id
+        self.age = age
+
+    def See(self):
+        print(f"Name= {self.__name},Age = {self.age} Id = {self.__Id}")
+    def setName(self,name):
+        self.__name = name
+    def updateName(self):
+        return self.__name
+
+
+
+#=======================================================
+student = Student("Shoykat ","22",182)
+student.See()
+'''student.age = 24
+student.See()
+student.__Id = 34
+student.See()'''
+student.setName("Alex")
+print(student.updateName())
+student.See()
