2d3401c664a4e2a5e8dd778f81b1f6746d6a9f49
Shoykat361 <shoykat15-11724@diu.edu.bd>
2023-12-18 01:09:08 +0600

    list done

---
+++ list.py
@@ -0,0 +1,29 @@
+x = [1, 2, 3, 4, 5, 6, 7, 8]
+'''a = list(x.reverse())
+print(a)'''
+a = list(reversed(x))  # Creates a reversed copy of x
+print(a)
+print(x)
+x.pop(0)
+print(x)
+x.append("alex")
+print(x)
+'''x.__add__(1,"Saha")
+print(x)'''
+
+del x[len(x) - 1]
+print(x)
+x.pop()
+print(x)
+
+list1 = x.copy()
+print(list1)
+print(list1 + x)
+list1.extend(x)
+print(list1)
+list2 = [2, 3, 4, 5, 6, 7, 8, 9, 0, 3, 2, 4, 5, 5, 4, 4, 3, 4, 56, 6, 4, 3, 3, 2, 2, 1, 2, 3, 5, 6, 7, 8, 8, 3]
+print(list2.count(3))
+list2 = [1,2,3,4,5,5,6,7,8,8,9,10]
+print(list2.count(1))
+list2.insert(2,2.5)
+print(list2)
