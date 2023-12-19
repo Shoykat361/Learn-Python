031723210d25c8cdd13dc07005d79d1c7c9addaa
Shoykat361 <shoykat15-11724@diu.edu.bd>
2023-12-19 23:21:10 +0600

    learn js

--- alex.py
+++ alex.py
@@ -92,3 +92,301 @@ num1 = Student(10)
 num2 = Student(5)
 print(num1+num2)
 print(num1 < num2)
+
+// var
+twoSum = function(nums, target)
+{
+// let
+firstInd
+//
+for (let index = 0; index < nums.length; index++) {
+
+                                                  // firstInd = nums[index]
+
+//}
+// console.log(firstInd)
+
+//};
+
+// console.log(twoSum([2, 7, 11, 15], 9))
+
+// function
+abc(str, char)
+{
+
+// let
+count = 0
+        //
+for (let val of str){
+                    // val === char & & count++;
+//}
+//
+return count
+//}
+
+// console.log(abc('ridoyyy', 'o'))
+
+function
+abc(str)
+{
+
+let
+lowerCase = str.toLocaleLowerCase()
+
+let
+result = lowerCase.split(' ')
+
+for (let index = 0; index < result.length; index++) {
+                                                    // result[index] = result[index][0].toUpperCase()
+console.log(result[index])
+result[index] = result[index][0].toUpperCase() + result[index].slice(1)
+
+}
+return result.join(' ')
+
+}
+
+console.log(abc('The name of out country name is bangladesh'))
+
+// this is a
+new
+json
+file
+
+let
+a = 'this is world and it is larger'
+console.log(a.slice(5))
+
+function
+removeDuplicate(nums)
+{
+
+let
+uniqueArr = [];
+
+for (let index = 0; index < nums.length; index++) {
+    const element = nums[index];
+
+if (!uniqueArr.includes(element)) {
+uniqueArr.push(element);
+}
+
+}
+console.log(uniqueArr)
+return uniqueArr;
+}
+
+console.log(removeDuplicate([1, 2, 3, 4, 2, 4, 5, 6, 1, 1]));
+
+// Question: here
+two
+array
+num1 = [1, 2, 3, 4, 5] and num2 = [1, 2, 3, 1, 2, 1, 3, 4, 5, 2, 3, 4, 1].out
+this
+answer
+how
+many
+time
+first
+array
+number
+have
+second
+number
+// time
+complexity
+O(n + n) = > O(n)
+function
+countArray(num1, num2)
+{
+let
+trackCountArray = {};
+
+for (let numElement of num1) {
+
+    trackCountArray[numElement]
+
+}
+
+for (let countElement of num2) {
+if (countElement in trackCountArray) {
+trackCountArray[countElement]++;
+} else {
+trackCountArray[countElement] = 1;
+}
+}
+
+return trackCountArray;
+}
+
+console.log(countArray([1, 2, 3, 4, 5], [1, 2, 3, 4, 5, 1, 2, 3, 3, 3, 3, 5]));
+
+// Q: Write
+a
+program
+that
+prints
+the
+numbers
+from
+
+1
+to
+100.But
+for multiple of three print "Fizz" instead of the number and for the multiple of five print "Buzz".FOr numbers which are multiples of both and five print "FIzzBuzz"
+
+function calculate(){
+let
+obj = {}
+for (let index = 1; index <= 60; index++) {
+    const element = index
+obj.element = element
+
+if (element % 3 == 0 & & element % 5 == 0){
+obj.element = "FizzBuzz"
+} else if (element % 3 == 0) {
+obj.element = "Fizz"
+
+} else if (element % 5 == 0){
+obj.element = "Buzz"
+}
+console.log(obj)
+}
+
+}
+
+calculate()
+
+// Adding
+Sum
+from an array
+// you
+have
+been
+provide
+an
+array
+of
+numbers
+// you
+have
+to
+sum
+the
+numbers
+
+function
+addNumber(num)
+{
+let
+sum = 0
+for (let index = 0; index < num.length; index++) {
+
+    sum += num[index]
+
+}
+return sum
+}
+
+console.log(addNumber([1, 2, 3, 4, 5, 10, 20]))
+
+// give
+an
+array
+finding
+maximun
+number and minimun
+number
+function
+findingMax(num)
+{
+let
+max_num = num[0]
+for (let index = 0; index < num.length; index++) {
+    let elementVal = num[index]
+
+if (elementVal > max_num){
+max_num =elementVal
+}
+
+}
+return max_num
+}
+
+console.log(findingMax([2, 3, 1, 5, 10, 36, 4, 9, 21, 11]))
+
+// give
+an
+array
+finding
+minimun
+number and minimun
+number
+function
+findingMin(num)
+{
+let
+min_num = num[0]
+for (let index = 0; index < num.length; index++) {
+    let elementVal = num[index]
+
+if (elementVal < min_num){
+min_num =elementVal
+}
+
+}
+return min_num
+}
+
+console.log(findingMin([2, 3, 1, 7]))
+
+// string
+reverse
+function
+strReverse(str)
+{
+let
+reversedString = '';
+for (let index = str.length - 1; index >= 0; index--) {
+    reversedString += str[index];
+}
+return reversedString;
+}
+
+console.log(strReverse('Bangladesh'));
+
+// another
+way
+function
+strReverse(str)
+{
+let
+newstr = str.split('')
+let
+rev = newstr.reverse()
+let
+val = rev.join('')
+
+return val;
+}
+
+console.log(strReverse('Bangladesh'));
+
+// integer
+reverse
+function
+strReverse(int)
+{
+let
+reversedString = '';
+for (let index = int.length - 1; index >= 0; index--) {
+    reversedString += int[index];
+}
+return reversedString;
+}
+
+console.log(strReverse([1, 2, 3, 4, 5]));
+
+
+
+
--- exercise.py
+++ exercise.py
@@ -8,6 +8,46 @@ print("Welcome to", end = ' ')
 print("GeeksforGeeks", end= ' ')
 '''
 
-x = 3.2
+'''x = 3.2
 print(x.__ceil__())
 print(x.__floor__())
+
+
+a = [1,2,3,4,5,6,7,8]
+b = [1,2,3,4,5,6,7,8]
+print(a,b)
+a.append(12)
+print(a)
+m = (a+b)
+print(m)'''
+'''import array
+
+my_Array = array.array('i', [1, 2, 3, 4])
+for i in my_Array:
+    print(i)
+print(my_Array)
+alist = ["saha", 1, 2, 3, 5.6]
+print(alist)
+
+print((lambda p, q: p * p + q * q) (2,4))
+numbers_xrange = 
+#print((lambda x, y: x * x + y * y)(2, 3))
+'''
+
+'''import array as  arr
+x = [1,2,3,4,5,6,7]
+
+a=arr.array('d', [1.1 , 2.1 ,3.1] )
+a.append(3.4)
+print(a)
+a.extend([4.5,6.3,6.8])
+print(a)
+a.insert(2,3.8)
+print(a)
+'''
+
+a = [1,2,3,4,5,6]
+a.remove(a[0])
+print(a)
+print(a.pop(a[0]))
+print(a)
